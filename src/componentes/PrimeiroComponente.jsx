895825c008c31296bee921f6960c7bebeba7af7a
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-16 14:13:19 -0300

    add: primeiroComponente

---
+++ src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () =>
+    <div>
+        <h1>Primeiro Componente (Arrow)!</h1>
+    </div>
+
+// export default function() {
+//     return <h1>Primeiro Componente!</h1>
+// }
+
+// function primeiro() {
+//     return <h1>Primeiro Componente!</h1>
+// }
+
+// export default primeiro
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
 
 const elemento = document.getElementById('root')
-ReactDOM.render(<h1>Olá React</h1>, elemento)
+ReactDOM.render(<div>
+    <PrimeiroComponente></PrimeiroComponente>
+</div>, elemento)
