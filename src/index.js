6562c6fb8940b145e91cefe1840f1a452b67744f
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-16 14:27:39 -0300

    add: doisComponentes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PrimeiroComponente from './componentes/PrimeiroComponente'
+//import PrimeiroComponente from './componentes/PrimeiroComponente'
+import { CompA , CompB as B} from './componentes/DoisComponentes'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(<div>
-    <PrimeiroComponente></PrimeiroComponente>
+    <CompA valor="Olá eu sou A!"></CompA>
+    <B valor="B na área!"></B>
+    {/*PrimeiroComponente valor='Bom dia!'></PrimeiroComponente>*/}
 </div>, elemento)
