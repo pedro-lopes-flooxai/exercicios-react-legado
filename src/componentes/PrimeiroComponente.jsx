e7294c08366c86f38a059ff5c63916d0ece75735
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-16 14:15:57 -0300

    add: props

--- src/componentes/PrimeiroComponente.jsx
+++ src/componentes/PrimeiroComponente.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-export default () =>
+let isLegal = true
+
+export default (props) => 
     <div>
-        <h1>Primeiro Componente (Arrow)!</h1>
+        <h1>{props.valor}</h1>
+        <h2>{1 + 1}</h2>
+        <p>Legal? {isLegal ? 'Sim' : 'Não'}</p>
+        <p>{Math.random()}</p>
     </div>
 
+//  export default () => 
+//     <div>
+//         <h1>Primeiro Componente (Arrow)!</h1>
+//     </div>
+    
 // export default function() {
 //     return <h1>Primeiro Componente!</h1>
 // }
