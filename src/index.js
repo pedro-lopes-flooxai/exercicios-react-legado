f8e47f87a2ebfa4c70a55c70831e06b00811aa88
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-16 15:40:28 -0300

    add: pai and filho

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import ReactDOM from 'react-dom'
 //import FamiliaSilva from './componentes/FamiliaSilva'
 //import Familia from './componentes/Familia'
 //import Membro from './componentes/Membro'
-import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+//import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+import Pai from './componentes/Pai'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(<div>
-    <ComponenteComFuncao></ComponenteComFuncao>
+    <Pai></Pai>
+    {/*<ComponenteComFuncao></ComponenteComFuncao>*/}
     {/*<Familia sobrenome="Pereira">
         <Membro nome="Andre"> </Membro>
         <Membro nome="Mariana"> </Membro>
