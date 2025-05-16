998b76d062adb704c7be413102291a0877dcc543
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-05-16 15:02:38 -0300

    add: familia and membro

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //import PrimeiroComponente from './componentes/PrimeiroComponente'
-import CompA , {CompB as B} from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
+//import CompA , {CompB as B} from './componentes/DoisComponentes'
+//import MultiElementos from './componentes/MultiElementos'
+//import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(<div>
-    <MultiElementos></MultiElementos>
-    <CompA valor="Olá eu sou A!"></CompA>
-    <B valor="B na área!"></B>
+    <Familia>
+        <Membro nome="Andre" sobrenome="Pereira"></Membro>
+        <Membro nome="Mariana" sobrenome="Pereira"></Membro>
+    </Familia>
+    <Familia>
+        <Membro nome="Bia" sobrenome="Arruda"></Membro>
+        <Membro nome="Gustavo" sobrenome="Arruda"></Membro>
+    </Familia>
+    {/*<FamiliaSilva></FamiliaSilva>*/}
+   { /*<MultiElementos></MultiElementos>*/}
+    {/*<CompA valor="Olá eu sou A!"></CompA>
+    <B valor="B na área!"></B>*/}
     {/*PrimeiroComponente valor='Bom dia!'></PrimeiroComponente>*/}
 </div>, elemento)
